4ab36f3f5124e842ddab74bf1e1af9e248250cd8
Eko Andri Subarnanto <eko.andri@icloud.com>
2019-01-17 07:52:56 +0700

    Done

--- src/App.js
+++ src/App.js
@@ -5,39 +5,46 @@ import { getProducts, getDummy } from "./fetch/GetData";
 import Config from "./fetch/Config";
 import "./App.css";
 
-
-const { Meta } = Card;
-
 const URL =
   process.env.NODE_ENV === "development" ? Config.prodURL : Config.devURL;
 
 class App extends Component {
   state = {
-    products: []
+    products: [],
+    dummyProducts: []
   };
 
   componentDidMount() {
-    console.log(URL);
     // this.getDataProducts();
     this.getDummyData();
   }
 
-  componentDidUpdate(){
-    this.getDummyData();
+  componentDidUpdate() {
+    // this.getDummyData();
   }
 
   getDummyData = () => {
     getDummy().then(result => {
-      console.log(result);
+      console.log(result.data);
+      this.setState({
+        dummyProducts: result.data.map(data => ({
+          dummy_ID: Math.random(new Date()),
+          image: data.image,
+          price: data.price,
+          currency: data.currency,
+          name: data.name
+        }))
+      });
     });
+    return null;
   };
 
   getDataProducts = () => {
     getProducts(URL).then(result => {
-      // const data = result.data[0];
       console.log(result.data);
       this.setState({
         products: result.data.map(data => ({
+          dummy_ID: Math.random(new Date()),
           image: data.image,
           price: data.price,
           currency: data.currency,
@@ -50,26 +57,31 @@ class App extends Component {
 
   render() {
     return (
-      <div style={{ background: "#ECECEC", padding: "30px" }}>
-        <Row gutter={16}>
-          {this.state.products.map(data => {
+      <div style={{ padding: "30px" }}>
+        <p className="best-mall">The Best Shopping Mall in The World</p>
+        <Row gutter={16} type="flex">
+          {this.state.dummyProducts.map(data => {
             return (
-              <Col span={8}>
+              <Col span={4} key={data.dummy_ID}>
                 <Card
                   hoverable
-                  style={{ width: 240 }}
+                  // style={{ height: 240 }}
                   cover={
                     <img
                       alt="example"
-                      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+                      src={data.image}
+                      height="240px"
+                      width="auto"
                     />
                   }
                 >
-                  <Meta
-                    title="Europe Street beat"
-                    description="www.instagram.com"
-                  />
-                  > Card content
+                  <div className="name-price">
+                    <p style={{ color: "orange" }}>{data.name}</p>
+                    <p>
+                      from {data.currency}{" "}
+                      <span style={{ fontSize: 30 }}>{data.price}</span>
+                    </p>
+                  </div>
                 </Card>
               </Col>
             );
